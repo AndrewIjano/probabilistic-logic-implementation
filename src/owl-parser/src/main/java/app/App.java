f89ff1732fa02969adcb4a39531ff4dd8e5679a5
Andrew Ijano <andrew.lopes@uol.com.br>
2019-08-22 12:31:19 -0300

    Add inicial drafts for owl parser

--- src/owl-parser/src/main/java/app/App.java
+++ src/owl-parser/src/main/java/app/App.java
@@ -1,13 +1,23 @@
 package app;
 
+import java.io.File;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+
+    public static void main(String[] args) throws Exception {
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+        File file = new File("pizza.owl");
+        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
+        
+        System.out.println("Hello World!");
     }
 }
