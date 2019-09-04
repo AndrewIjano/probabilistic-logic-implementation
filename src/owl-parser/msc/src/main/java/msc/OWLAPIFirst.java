4884233e3962ddf21737dd1752cb28042e3be5cc
Andrew Ijano <andrew.lopes@uol.com.br>
2019-09-04 14:13:34 -0300

    Add axiom printing

--- src/owl-parser/msc/src/main/java/msc/OWLAPIFirst.java
+++ src/owl-parser/msc/src/main/java/msc/OWLAPIFirst.java
@@ -1,16 +1,11 @@
 package msc;
 
 import java.io.File;
-import java.util.Collections;
-
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.util.OWLOntologyWalker;
-import org.semanticweb.owlapi.util.OWLOntologyWalkerVisitor;
 
 public class OWLAPIFirst {
 	public static final File pizza_file = new File("pizza.owl");
@@ -20,20 +15,9 @@ public class OWLAPIFirst {
 		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
 
 		OWLOntology o = man.loadOntologyFromOntologyDocument(pizza_file);
-
-		// Create the walker
-		OWLOntologyWalker walker = new OWLOntologyWalker(Collections.singleton(o));
-		// Now ask our walker to walk over the ontology
-		OWLOntologyWalkerVisitor visitor = new OWLOntologyWalkerVisitor(walker) {
-			public void visit(OWLObjectSomeValuesFrom desc) {
-				System.out.println(desc);
-				System.out.println(" " + getCurrentAxiom());
-				return;
-			}
-		};
-		// Have the walker walk...
-		walker.walkStructure(visitor);
-
+		
+		o.logicalAxioms().forEach((p) -> System.out.println(p));
+		
 		System.out.println(o);
 	}
 }
